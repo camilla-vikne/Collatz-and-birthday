97ade7206e60c1eed112c8e181e1e3f9c19a7805
andreasb09 <andreasb09.kodehode@gmail.com>
2023-12-11 11:45:37 +0100

    added Y runs

--- birthday.js
+++ birthday.js
@@ -1,8 +1,16 @@
 const probability = {
-  exponentiation: function (d, n) {
-    const a = (n * (n - 1)) / 2,
-      b = (d - 1) / d;
-    return ((1 - Math.pow(b, a)) * 100).toFixed(2);
+  exponentiation: function (d, n, Y) {
+    let totalProbability = 0;
+
+    for (let i = 0; i < Y; i++) {
+      const a = (n * (n - 1)) / 2,
+        b = (d - 1) / d;
+      const result = (1 - Math.pow(b, a)) * 100;
+      totalProbability += result;
+    }
+
+    const averageProbability = (totalProbability / Y).toFixed(2);
+    return averageProbability;
   },
 };
 
@@ -13,6 +21,7 @@ button.addEventListener("click", onClick);
 function onClick(event) {
   const days = 365,
     people = document.getElementById("people").value;
+  runs = document.getElementById("runs").value;
 
   document.getElementById("results").innerHTML =
     "The probability of some pair having the same birthday in a set of " +
