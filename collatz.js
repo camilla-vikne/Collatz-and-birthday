a1d310484cb42d75d3d330afe2c8697001efd6e0
KristianB09 <kristianb09.kodehode@gmail.com>
2023-12-14 16:25:22 +0100

    removed aquisition JS file and put new code in collatz.js

--- collatz.js
+++ collatz.js
@@ -1,10 +1,13 @@
-function collatz() {
+collatz = () => {
   let inputNumber = document.getElementById("inputValue").value;
 
   collatzFunction(inputNumber);
-}
+};
+
+let countArray = [];
 
 function collatzFunction(n) {
+  countArray = [];
   n = parseInt(n, 10);
   const initalValue = n;
   let count = 0;
@@ -19,13 +22,42 @@ function collatzFunction(n) {
       count++;
       console.log(n);
       maxNumber = Math.max(maxNumber, n);
+      countArray.push({ count: count, value: n });
     }
     document.getElementById(
       "output"
     ).innerHTML = `${initalValue} used ${count} steps to reach 1, and the highest value reached was ${maxNumber}`;
+    console.log(countArray);
   } else {
     document.getElementById(
       "output"
     ).innerHTML = `please enter a valid integer number`;
   }
+  destroyChart();
+  createChart();
+}
+
+let myChart;
+
+function destroyChart() {
+  if (myChart != null) {
+    myChart.destroy();
+  }
+}
+
+function createChart() {
+  const data = countArray;
+
+  myChart = new Chart(document.getElementById("acquisitions"), {
+    type: "line",
+    data: {
+      labels: data.map((row) => row.count),
+      datasets: [
+        {
+          label: "Value",
+          data: data.map((row) => row.value),
+        },
+      ],
+    },
+  });
 }
